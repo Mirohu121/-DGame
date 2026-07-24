aad2986fd8e69ce015ee1c90d8859f1d5b400abc
Miwa <>
2026-07-24 10:57:55 +0900

    作業内容 金色のマテリアルを作成 itemスクリプトを作成しキューブにアタッチしてふわふわしながら回転するように実装

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Item : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Yé≤Ç…âiâìÇ…âÒì] (DOTween)
+        transform.DORotate(new Vector3(0, 360, 0), 2.0f, RotateMode.FastBeyond360)
+                 .SetLoops(-1, LoopType.Restart)
+                 .SetEase(Ease.Linear);
+
+        // è„â∫Ç…Ç”ÇÌÇ”ÇÌïÇóV (DOTween)
+        transform.DOMoveY(transform.position.y + 0.5f, 1.0f)
+                 .SetLoops(-1, LoopType.Yoyo)
+                 .SetEase(Ease.InOutSine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
